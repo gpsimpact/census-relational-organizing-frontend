81a14374a587d0cc691b0a8a4f004a450fed85ed
jakelowen <jakelowen@gmail.com>
2019-03-07 16:40:12 -0600

    fixing port issue

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,27 @@
 import * as express from "express";
 import * as next from "next";
 
-const port = parseInt(process.env.PORT!, 10) || 3000;
+const PORT = process.env.PORT || 5000;
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.prepare()
-.then(() => {
-  const server = express()
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  });
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
 
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+    server.listen(PORT, err => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${PORT}`);
+    });
   })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
