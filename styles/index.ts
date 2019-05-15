3a9e032ea31ea02d2189bde13d191a4cfd5ffb45
joshuamitchell <joshua@gpsimpact.com>
2019-05-15 09:39:12 -0500

    rebasing

--- styles/index.ts
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "../lib/styled";
-import { GlobalStyle } from './Style';
-import { Animations } from './Animations';
-import { Grid } from './Grid';
-export const AddGlobals = createGlobalStyle`
-  ${Grid};
-  ${GlobalStyle};
-  ${Animations};
-`;
