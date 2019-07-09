801e341d04a7bec8c499b705f59cb3fe5eccdfd4
joshuamitchell <joshua@gpsimpact.com>
2019-07-09 10:26:20 -0500

    fixing toggle

--- components/Filters/FilterToggler.js
+++ components/Filters/FilterToggler.js
@@ -1,6 +1,29 @@
 import { Mutation, Query } from 'react-apollo';
 import { FILTERS_OPEN_QUERY, TOGGLE_FILTERS_OPEN_MUTATION } from '../Queries/FilterContainer';
-import Nav from 'react-bootstrap/Nav';
+import styled from 'styled-components';
+
+const Toggle = styled('a')`
+        -webkit-transition: .25s ease-in-out;
+        -moz-transition: .25s ease-in-out;
+        -o-transition: .25s ease-in-out;
+        transition: .25s ease-in-out;
+    padding: 10px 10px;
+    display: inline-block;
+    background-color: ${props => props.theme.colors.g0};
+    color: ${props => props.theme.colors.tertiary};
+    border: 1px solid ${props => props.theme.colors.g2};
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: .8rem;
+    &:hover,
+    &:active,
+    &:focus {
+        background-color: ${props=>props.theme.colors.primary};
+        color: ${props => props.theme.colors.white} !important;
+        cursor: pointer;
+    }
+
+`;
 
 export const FilterToggler = (props) => {
     return(
@@ -10,9 +33,9 @@ export const FilterToggler = (props) => {
                     <Query query={FILTERS_OPEN_QUERY}>
                         {({data: {filtersOpen}}) => {
                             return(
-                                <Nav.Link onClick={() => toggleFilters()} open={filtersOpen}>
+                                <Toggle onClick={() => toggleFilters()} open={filtersOpen}>
                                     <i className="fas fa-filter"></i> Filter
-                                </Nav.Link>
+                                </Toggle>
                             )
                         }}
                     </Query>
