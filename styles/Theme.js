3b45e43c579b790ed66354d2ac4890f4afe5a68b
joshuamitchell <joshua@gpsimpact.com>
2019-10-04 14:32:50 -0500

    don't break word

--- styles/Theme.js
+++ styles/Theme.js
@@ -43,9 +43,7 @@ export const GlobalStyle = css`
     white-space: -pre-wrap;      /* Opera 4-6 */
     white-space: -o-pre-wrap;    /* Opera 7 */
     white-space: pre-wrap;       /* css-3 */
-    word-wrap: break-word;       /* Internet Explorer 5.5+ */
     white-space: -webkit-pre-wrap; /* Newer versions of Chrome/Safari*/
-    word-break: break-all;
     white-space: normal;
     }
 
